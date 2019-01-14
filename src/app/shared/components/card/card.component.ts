588899b5c53e4d43d6262adf85a3c01929f04807
Graham Hency <graham.hency@tangoe.com>
2019-01-14 16:42:02 -0500

    Created a 'go-card' component

---
+++ src/app/shared/components/card/card.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input, ViewEncapsulation } from '@angular/core';
+
+@Component({
+ encapsulation: ViewEncapsulation.None,
+ selector: 'go-card',
+ styleUrls: ['./card.component.scss'],
+ templateUrl: './card.component.html'
+})
+
+export class CardComponent { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { CardComponent } from './components/card/card.component';
 import { IconComponent } from './components/icon/icon.component';
 
 @NgModule({
   declarations: [
+    CardComponent,
     IconComponent
   ],
   exports: [
+    CardComponent,
     IconComponent
   ],
   imports: [
