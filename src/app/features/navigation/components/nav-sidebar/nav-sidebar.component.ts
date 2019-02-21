81cca1210280986ff7b44ddd34b3a6001cedd0e6
Graham Hency <graham.hency@tangoe.com>
2019-02-21 13:30:52 -0500

    Commenting out elements for now, until we have use for them.
    Added correct logo.

--- src/app/features/navigation/components/nav-sidebar/nav-sidebar.component.ts
+++ src/app/features/navigation/components/nav-sidebar/nav-sidebar.component.ts
@@ -14,12 +14,12 @@ export class NavSidebarComponent implements OnInit {
     { route: 'ui-kit', routeIcon: 'widgets', routeTitle: 'Components', subRoutes: [
       { route: 'ui-kit/accordion', routeTitle: 'Accordion' },
       { route: 'ui-kit/accordion-panel', routeTitle: 'Accordion Panel' },
-      { route: 'ui-kit/table', routeTitle: 'Table'}
+      // { route: 'ui-kit/table', routeTitle: 'Table'}
     ] },
-    { route: 'patterns', routeIcon: 'view_quilt', routeTitle: 'Patterns' },
-    { route: 'assets', routeIcon: 'photo_library', routeTitle: 'Assets' },
-    { route: 'themes', routeIcon: 'format_paint', routeTitle: 'Themes' },
-    { route: 'branding', routeIcon: 'domain', routeTitle: 'Branding' }
+    // { route: 'patterns', routeIcon: 'view_quilt', routeTitle: 'Patterns' },
+    // { route: 'assets', routeIcon: 'photo_library', routeTitle: 'Assets' },
+    // { route: 'themes', routeIcon: 'format_paint', routeTitle: 'Themes' },
+    // { route: 'branding', routeIcon: 'domain', routeTitle: 'Branding' }
   ]
 
   ngOnInit() { }
