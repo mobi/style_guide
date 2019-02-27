4848768d1e509b244ee3c3b17112455e97f67b1a
Graham Hency <graham.hency@tangoe.com>
2019-02-27 09:44:49 -0500

    Added documenation for the `<go-button>`

--- src/app/features/ui-kit/routes/ui-kit-routing.module.ts
+++ src/app/features/ui-kit/routes/ui-kit-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AccordionDocsComponent } from '../components/accordion-docs/accordion-docs.component';
 import { AccordionPanelDocsComponent } from '../components/accordion-panel-docs/accordion-panel-docs.component';
+import { ButtonDocsComponent } from '../components/button-docs/button-docs.component';
 import { CardDocsComponent } from '../components/card-docs/card-docs.component';
 import { IconDocsComponent } from '../components/icon-docs/icon-docs.component';
 import { TableDocsComponent } from '../components/table-docs/table-docs.component';
@@ -12,6 +13,7 @@ const routes: Routes = [
   { path: 'ui-kit', component: UiKitComponent },
   { path: 'ui-kit/accordion', component: AccordionDocsComponent },
   { path: 'ui-kit/accordion-panel', component: AccordionPanelDocsComponent },
+  { path: 'ui-kit/button', component: ButtonDocsComponent },
   { path: 'ui-kit/card', component: CardDocsComponent },
   { path: 'ui-kit/icon', component: IconDocsComponent },
   { path: 'ui-kit/table', component: TableDocsComponent }
--- src/app/features/ui-kit/ui-kit.module.ts
+++ src/app/features/ui-kit/ui-kit.module.ts
@@ -4,6 +4,7 @@ import { HighlightModule } from 'ngx-highlightjs';
 
 // GoPonents
 import { GoAccordionModule } from 'goponents';
+import { GoButtonModule } from 'goponents';
 import { GoCardModule } from 'goponents';
 import { GoIconModule } from 'goponents';
 
@@ -13,15 +14,17 @@ import { UiKitRoutesModule } from './routes/ui-kit-routing.module';
 // Module Components
 import { AccordionDocsComponent } from './components/accordion-docs/accordion-docs.component';
 import { AccordionPanelDocsComponent } from './components/accordion-panel-docs/accordion-panel-docs.component';
-import { TableDocsComponent } from './components/table-docs/table-docs.component';
-import { UiKitComponent } from './components/ui-kit/ui-kit.component';
+import { ButtonDocsComponent } from './components/button-docs/button-docs.component';
 import { CardDocsComponent } from './components/card-docs/card-docs.component';
 import { IconDocsComponent } from './components/icon-docs/icon-docs.component';
+import { TableDocsComponent } from './components/table-docs/table-docs.component';
+import { UiKitComponent } from './components/ui-kit/ui-kit.component';
 
 @NgModule({
   imports: [
     CommonModule,
     GoAccordionModule,
+    GoButtonModule,
     GoCardModule,
     GoIconModule,
     HighlightModule,
@@ -30,10 +33,11 @@ import { IconDocsComponent } from './components/icon-docs/icon-docs.component';
   declarations: [
     AccordionDocsComponent,
     AccordionPanelDocsComponent,
-    TableDocsComponent,
-    UiKitComponent,
+    ButtonDocsComponent,
     CardDocsComponent,
-    IconDocsComponent
+    IconDocsComponent,
+    TableDocsComponent,
+    UiKitComponent
   ]
 })
 
