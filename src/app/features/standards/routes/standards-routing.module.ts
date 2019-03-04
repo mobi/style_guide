7657c273854cabff3ce0e438f7b5b49e33528520
Alex Overbeck <alexoverbeck@gmail.com>
2019-03-04 15:39:44 -0500

    Add documentation and styles for typography

--- src/app/features/standards/routes/standards-routing.module.ts
+++ src/app/features/standards/routes/standards-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { StandardsComponent } from '../components/standards/standards.component';
+import { TypographyComponent } from '../components/typography/typography.component';
 
 const routes: Routes = [
-  { path: 'standards', component: StandardsComponent }
+  { path: 'standards', component: StandardsComponent },
+  { path: 'standards/typography', component: TypographyComponent }
 ];
 
 @NgModule({
