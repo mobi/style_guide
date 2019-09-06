2aec1d5915346373e913eb40d4b56e1c9442e109
Alex Overbeck <AlexOverbeck@users.noreply.github.com>
2019-09-06 09:25:28 -0400

    Document forms overview (#50)
    The forms overview gives a breif explantion of reactive forms.

---
+++ src/app/features/ui-kit/components/form-docs/form-docs.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+import { NavGroup } from '@tangoe/goponents/lib/components/go-side-nav/nav-group.model';
+
+@Component({
+  templateUrl: './form-docs.component.html'
+})
+export class FormDocsComponent {
+  menuItems: Array<NavGroup> = [
+    {
+      routeTitle: 'Basics', subRoutes: [
+        { route: './', routeTitle: 'Overview' },
+      ]
+    }
+  ];
+}
