7b4f04a9882f279860d1acf0fad071e8019a7147
Steven Ulmer <steven.ulmer@tangoe.com>
2019-09-14 10:59:37 -0400

    add docs for copy button

--- src/app/features/ui-kit/ui-kit.module.ts
+++ src/app/features/ui-kit/ui-kit.module.ts
@@ -9,6 +9,7 @@ import {
   GoBadgeModule,
   GoButtonModule,
   GoCardModule,
+  GoCopyModule,
   GoDatepickerModule,
   GoIconModule,
   GoLoaderModule,
@@ -29,11 +30,16 @@ import { UiKitRoutesModule } from './routes/ui-kit-routing.module';
 // Module Components
 import { AccordionDocsComponent } from './components/accordion-docs/accordion-docs.component';
 import { AccordionPanelDocsComponent } from './components/accordion-panel-docs/accordion-panel-docs.component';
+import { BadgeDocsComponent } from './components/badge-docs/badge-docs.coponent';
 import { ButtonDocsComponent } from './components/button-docs/button-docs.component';
 import { CardDocsComponent } from './components/card-docs/card-docs.component';
+import { CopyDocsComponent } from './components/copy-docs/copy-docs.component';
 import { DatepickerDocsComponent } from './components/form-docs/components/datepicker-docs/datepicker-docs.component';
+import { FormControlDocsComponent } from './components/form-docs/components/form-control-docs/form-control-docs.component';
 import { FormDocsComponent } from './components/form-docs/form-docs.component';
+import { FormsOverviewComponent } from './components/form-docs/components/forms-overview/forms-overview.component';
 import { IconDocsComponent } from './components/icon-docs/icon-docs.component';
+import { InputDocsComponent } from './components/form-docs/components/input-docs/input-docs.component';
 import { ModalDocsComponent } from './components/modal-docs/modal-docs.component';
 import { ModalTestComponent } from './components/modal-test/modal-test.component';
 import { ServerIntegrationComponent } from './components/table-docs/components/server-integration/server-integration.component';
@@ -53,11 +59,7 @@ import { LayoutNavComponent } from './components/layout-docs/components/layout-n
 import { LayoutHeaderComponent } from './components/layout-docs/components/layout-header/layout-header.component';
 import { LayoutSearchComponent } from './components/layout-docs/components/layout-search/layout-search.component';
 import { LayoutExampleComponent } from './components/layout-docs/components/layout-example/layout-example.component';
-import { FormsOverviewComponent } from './components/form-docs/components/forms-overview/forms-overview.component';
-import { InputDocsComponent } from './components/form-docs/components/input-docs/input-docs.component';
 import { TextAreaDocsComponent } from './components/form-docs/components/text-area-docs/text-area-docs.component';
-import { FormControlDocsComponent } from './components/form-docs/components/form-control-docs/form-control-docs.component';
-import { BadgeDocsComponent } from './components/badge-docs/badge-docs.coponent';
 
 @NgModule({
   imports: [
@@ -66,6 +68,7 @@ import { BadgeDocsComponent } from './components/badge-docs/badge-docs.coponent'
     GoBadgeModule,
     GoButtonModule,
     GoCardModule,
+    GoCopyModule,
     GoDatepickerModule,
     GoIconModule,
     GoInputModule,
@@ -86,6 +89,7 @@ import { BadgeDocsComponent } from './components/badge-docs/badge-docs.coponent'
     BadgeDocsComponent,
     ButtonDocsComponent,
     CardDocsComponent,
+    CopyDocsComponent,
     FormControlDocsComponent,
     DatepickerDocsComponent,
     FormDocsComponent,
